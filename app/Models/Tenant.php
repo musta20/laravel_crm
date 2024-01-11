5a5f9bcbdd5a294750814170ffac1461d029f3a4
musta20 <saif.muh2020@gmail.com>
2024-01-12 01:58:21 +0200

    adding laravel tenant packge and create tenant model

---
+++ app/Models/Tenant.php
@@ -0,0 +1,15 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Models;
+
+use Stancl\Tenancy\Database\Models\Tenant as BaseTenant;
+use Stancl\Tenancy\Contracts\TenantWithDatabase;
+use Stancl\Tenancy\Database\Concerns\HasDatabase;
+use Stancl\Tenancy\Database\Concerns\HasDomains;
+
+class Tenant extends BaseTenant implements TenantWithDataBase
+{
+    use HasDatabase;
+    use HasDomains;
+}
